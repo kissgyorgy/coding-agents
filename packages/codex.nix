1220a1777ddb66e3767ab842ab8ebac624c07b82
György Kiss <gyorgy@duck.com>
2025-11-10 11:50:55 +0100

    Updated codex to v0.57.0

--- packages/codex.nix
+++ packages/codex.nix
@@ -12,7 +12,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://github.com/openai/codex/releases/download/${version}/codex-x86_64-unknown-linux-gnu.tar.gz";
-    hash = "sha256-Oq/U6nbz1ywVh3rjctoHI3fFhHdUdgawUiirxri7ERQ=";
+    hash = "sha256-EXsfp1xln1Dc0J0xM8/mUg9KnFRXrZCscJ1noZ7aNYw=";
   };
 
   nativeBuildInputs = [ autoPatchelfHook ];
