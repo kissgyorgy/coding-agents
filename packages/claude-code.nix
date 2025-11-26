d78a84e6061922c4991c774d5155f099edb5daad
György Kiss <gyorgy@duck.com>
2025-11-26 22:43:12 +0100

    Updated Claude Code to 2.0.54

--- packages/claude-code.nix
+++ packages/claude-code.nix
@@ -7,11 +7,11 @@
 
 stdenv.mkDerivation rec {
   pname = "claude-code";
-  version = "2.0.53";
+  version = "2.0.54";
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-    hash = "sha256-GDk3oROfwlreyZ95oWkUc/OAv8pRHhHLDcwmRzXq3Wg=";
+    hash = "sha256-EVZueeW1MewYmQSHp4flcShqHy5H0S4gET3XtK+ttQA=";
   };
 
   nativeBuildInputs = [ makeBinaryWrapper ];
