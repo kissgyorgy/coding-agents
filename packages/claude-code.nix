864112a9da34d7f0938a455ba0ec06144249ad1a
György Kiss <gyorgy@duck.com>
2025-12-05 23:23:16 +0100

    claude-code: 2.0.57 -> 2.0.59
    Diff: https://npmdiff.dev/@anthropic-ai%2Fclaude-code/2.0.57/2.0.59

--- packages/claude-code.nix
+++ packages/claude-code.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "claude-code";
-  version = "2.0.57";
+  version = "2.0.59";
 
   src = fetchurl {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-    hash = "sha256-P/L5NfHkNvCoSOvarSd2ygurDB57lBZWzjpuZNC3QEM=";
+    hash = "sha256-4iMadFiz7cEovWgRQcs9iuVTY5PTXd5kJATshRyeFgY=";
   };
 
   nativeBuildInputs = [ makeBinaryWrapper ];
