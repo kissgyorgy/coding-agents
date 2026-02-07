819b6da41bb5d08e69edfddd3a10b9c9039f6cdc
György Kiss <gyorgy@duck.com>
2026-02-07 13:59:43 +0100

    pi-coding-agent package

---
+++ packages/pi-coding-agent.nix
@@ -0,0 +1,44 @@
+{ lib, stdenv, fetchurl, makeWrapper }:
+
+stdenv.mkDerivation rec {
+  pname = "pi-coding-agent";
+  version = "0.52.7";
+
+  src = fetchurl {
+    url = "https://github.com/badlogic/pi-mono/releases/download/v${version}/pi-linux-x64.tar.gz";
+    hash = "sha256-+QRg0qXKViMDafDX8Nc+TjIrM0axKw8ZPIVyNY/wntE=";
+  };
+
+  sourceRoot = "pi";
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  dontPatchELF = true;
+  dontStrip = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/lib/pi-coding-agent $out/bin
+    cp -r ./* $out/lib/pi-coding-agent/
+
+    chmod u+w,+x $out/lib/pi-coding-agent/pi
+    patchelf --set-interpreter ${stdenv.cc.bintools.dynamicLinker} $out/lib/pi-coding-agent/pi
+
+    makeWrapper $out/lib/pi-coding-agent/pi $out/bin/pi \
+      --set PI_PACKAGE_DIR $out/lib/pi-coding-agent
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Minimal terminal coding harness with AI-powered agent capabilities";
+    homepage = "https://github.com/badlogic/pi-mono/tree/main/packages/coding-agent";
+    downloadPage = "https://github.com/badlogic/pi-mono/releases";
+    sourceProvenance = [ sourceTypes.binaryNativeCode ];
+    license = licenses.mit;
+    maintainers = [ ];
+    mainProgram = "pi";
+    platforms = [ "x86_64-linux" ];
+  };
+}
