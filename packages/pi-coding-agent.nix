0529d00e2ca8707f9cc577dacc392be74e571fe4
György Kiss <gyorgy@duck.com>
2026-02-13 00:45:26 +0100

    pi-coding-agent: 0.52.9 -> 0.52.10

--- packages/pi-coding-agent.nix
+++ packages/pi-coding-agent.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "pi-coding-agent";
-  version = "0.52.9";
+  version = "0.52.10";
 
   src = fetchurl {
     url = "https://github.com/badlogic/pi-mono/releases/download/v${version}/pi-linux-x64.tar.gz";
-    hash = "sha256-oTzIqkcuLNVb8QGvAImzAjGNCuB0OZtzz6NSvhzGxRQ=";
+    hash = "sha256-4pssFdLsirSUq09BvcbmZI4FKQu3jrFTJm5fotjpZbE=";
   };
 
   sourceRoot = "pi";
