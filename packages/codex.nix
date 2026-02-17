8825b19848b1d1eaa5da041704dc689ee89efe94
György Kiss <gyorgy@duck.com>
2026-02-17 21:20:08 +0100

    codex: rust-v0.101.0 -> rust-v0.102.0

--- packages/codex.nix
+++ packages/codex.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "codex";
-  version = "rust-v0.101.0";
+  version = "rust-v0.102.0";
 
   src = fetchurl {
     url = "https://github.com/openai/codex/releases/download/${version}/codex-x86_64-unknown-linux-gnu.tar.gz";
-    hash = "sha256-6XMt47hw32o5zkukRplhDvWBhDlneTRX+O8R86WlgjY=";
+    hash = "sha256-M97/fN+Vu1giO1fpqnTnpd9PUeLfefXAiB89jvlrrEI=";
   };
 
   nativeBuildInputs = [ autoPatchelfHook ];
