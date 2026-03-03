6949ecc4cfd195445a7f4d54b1bb02c7480f2f02
György Kiss <gyorgy@duck.com>
2026-03-03 08:10:19 +0100

    Updated pi models

--- packages/pi-coding-agent/default.nix
+++ packages/pi-coding-agent/default.nix
@@ -1,7 +1,7 @@
 { lib, buildNpmPackage, fetchFromGitHub, nodejs_22, makeBinaryWrapper, autoPatchelfHook, stdenv }:
 
 let
-  modelsDate = "20260227";
+  modelsDate = "20260303";
 in
 
 buildNpmPackage rec {
