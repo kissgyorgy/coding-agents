4da7e973fa9d9b635da235e8ca3abc4218b71b61
György Kiss <gyorgy@duck.com>
2026-04-03 14:41:36 +0200

    claude-code: 2.1.89 -> 2.1.91

--- packages/claude-code.nix
+++ packages/claude-code.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "claude-code";
-  version = "2.1.89";
+  version = "2.1.91";
 
   src = fetchurl {
     url = "https://storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases/${version}/linux-x64/claude";
-    hash = "sha256-kDyzyWsxTYaFZjLIcC9c35cbgE0LGe+HRGVzvNHX3xw=";
+    hash = "sha256-AbdOGwLjMwlAs1JtL24AvzL3/Z5rOGG+amHgHP1yluY=";
   };
 
   dontUnpack = true;
