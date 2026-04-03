1df6b053afd27692b246c315732a56fa0aef6b7c
György Kiss <gyorgy@duck.com>
2026-04-03 14:43:51 +0200

    crush: 0.54.0 -> 0.55.0

--- packages/crush.nix
+++ packages/crush.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "crush";
-  version = "0.54.0";
+  version = "0.55.0";
 
   src = fetchurl {
     url = "https://github.com/charmbracelet/crush/releases/download/v${version}/crush_${version}_Linux_x86_64.tar.gz";
-    hash = "sha256-N+I0mWaH64t8rGGSpgo2nAVmEFt6dE1bzKrd2mCd2Do=";
+    hash = "sha256-NYs7WuTGJ3heqRP3xRtWuVPX8f/84LDaa93tLxWQQ8o=";
   };
 
   nativeBuildInputs = [ autoPatchelfHook ];
