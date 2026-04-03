ce4545c8a2a388cc930ad659e121ee1ac4395b32
György Kiss <gyorgy@duck.com>
2026-04-03 14:44:04 +0200

    pi-coding-agent: update generated models

--- packages/pi-coding-agent/default.nix
+++ packages/pi-coding-agent/default.nix
@@ -1,7 +1,7 @@
 { lib, buildNpmPackage, callPackage, fetchFromGitHub, nodejs_22, makeBinaryWrapper, autoPatchelfHook, stdenv }:
 
 let
-  modelsDate = "20260401";
+  modelsDate = "20260403";
   fetchExtensionDeps = callPackage ./fetch-extension-deps.nix { };
 in
 
